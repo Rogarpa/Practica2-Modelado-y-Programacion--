2a75b1dc59eb583da19835c0d68ceee27c58fd70
juan759 <jasso.antonio2001@gmail.com>
2020-10-24 22:00:33 -0500

    Menús terminados en Robot.

--- src/Robot.java
+++ src/Robot.java
@@ -70,8 +70,9 @@ public class Robot{
 
     public void desplegarMenuAcciones(){
         int i = 0;
-
-        System.out.println("Digite el número de la opción y presione enter: ");
+        boolean dan=true;
+    do {
+        System.out.println("0.-Terminar menu");
         System.out.println("1.- Activarse");
         System.out.println("2.- Caminar");
         System.out.println("3.- Dirigirse al area de reabastecimiento");
@@ -79,12 +80,12 @@ public class Robot{
         System.out.println("5.- Reabastecer");
         System.out.println("6.- Construir");
         System.out.println("7.- Suspenderse");
-        i = obtenerDigito(1, 7, "Opcion incorrecta");
-
-        switch (i) {
+        switch(getint("Digite el número de la opción y presione enter:","No es un numero valido.")){
+            case 0:
+            dan=false;
+            break;
             case 1:
             activarse();
-            break;
             case 2:
             caminar();
             break;
@@ -103,17 +104,19 @@ public class Robot{
             case 7:
             suspenderse();
             break;
-
             default:
             System.out.println("Opción incorrecta");
             desplegarMenuAcciones();
+            break;
         }
+        break;
+    } while (dan==true);
     }
     private int obtenerDigito(int limiteInferior, int limiteSuperior, String mensajeError){
         Scanner sc = new Scanner(System.in);
         int digitoObtenido = limiteSuperior + 1;
         String bufferCleaner = "";
-        while(digitoObtenido < limiteInferior || digitoObtenido > limiteSuperior){
+        while(digitoObtenido <= limiteInferior || digitoObtenido > limiteSuperior){
             if(sc.hasNextInt()) digitoObtenido = sc.nextInt();
             else {
                 bufferCleaner = sc.nextLine();
@@ -122,7 +125,87 @@ public class Robot{
         }
         return digitoObtenido;
     }
+
+    public int getint(String a, String b){
+        int num=0;
+        boolean c=true;
+        Scanner scn=new Scanner(System.in);
+        do{
+            System.out.println(a);
+            if(scn.hasNextInt()){
+                num=scn.nextInt();
+                c=false;
+            }
+            else{
+                scn.next();
+                System.out.println(b);
+            }
+        }while(c);
+        return num;
+    }
+
     public void desplegarMenuRecepcionOrden(){
-        //FALTA
+        int m=0;
+        boolean dani=true;
+        String esqueleto="1 para el esqueleto de concreto y 2 para el esqueleto Reforzado.";
+        String aislamiento="1 para el aislamiento de vidrio, 2 para aislamiento de madera,3 para aislamiento de concreto y 4 para aislamiento reforzado.";
+        do {
+            switch (getint(esqueleto,"No es un número válido.")) {
+                case 1:
+                switch (getint(aislamiento,"No es un número válido.")) {
+                    case 1:
+                    //casa=new EsqueletoConcretoAislamientoVidrio();
+                    dani=false;
+                    break;
+                    case 2:
+                    //casa=new EsqueletoConcretoAislamientoMadera();
+                    dani=false;
+                    break;
+                    case 3:
+                    //casa=new EsqueletoConcretoAislamientoConcreto();
+                    dani=false;
+                    break;
+                    case 4:
+                    //casa=new EsqueletoConcretoAislamientoReforzado();
+                    dani=false;
+                    break;
+                    default:
+                    System.out.println("Opción incorrecta");
+                    desplegarMenuRecepcionOrden();
+                    break;
+                }
+                break;
+                case 2:
+                switch (getint(aislamiento,"No es un número válido.")) {
+                    case 1:
+                    //casa=new EsqueletoReforzadoAislamientoVidrioEsqReforzado();
+                    dani=false;
+                    break;
+                    case 2:
+                    //casa=new EsqueletoReforzadoAislamientoMaderaEsqReforzado();
+                    dani=false;
+                    break;
+                    case 3:
+                    //casa=new EsqueletoReforzadoAislamientoConcretoEsqReforzado();
+                    dani=false;
+                    break;
+                    case 4:
+                    //casa=new EsqueletoReforzadoAislamientoReforzadoEsqReforzado();
+                    dani=false;
+                    break;
+                    default:
+                    System.out.println("Opción incorrecta");
+                    desplegarMenuRecepcionOrden();
+                    break;
+                }
+                break;
+                default:
+                System.out.println("Opción incorrecta");
+                desplegarMenuRecepcionOrden();
+                break;
+            }
+
+        } while (dani==true);
+
     }
 }
