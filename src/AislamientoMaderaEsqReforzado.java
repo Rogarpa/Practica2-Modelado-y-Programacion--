ae43a452f5e941b17894fe1824449ff40f96657d
juan759 <jasso.antonio2001@gmail.com>
2020-10-24 23:37:58 -0500

    Arreglado un problema de conflictos.

--- src/AislamientoMaderaEsqReforzado.java
+++ src/AislamientoMaderaEsqReforzado.java
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-public class AislamientoMaderaEsqReforzado extends EsqueletoReforzado{
-    
-    int PrecioAislamientoMadera=1000;
 
-    public void aislamiento(){
-        System.out.println("Construyendo aislamiento de Madera.");
-        Precio +=PrecioAislamientoMadera;
-=======
  public class AislamientoMaderaEsqReforzado extends EsqueletoReforzado{
     int PrecioAislamientoMadera=1000;
 
     public void aislamiento(){
         System.out.println("Construyendo aislamiento de Madera");
         Precio += PrecioAislamientoMadera;
->>>>>>> 86455c419ebb0b9bb87411e146da92f50b789f07
     }
 }
