7c9151c4c71a73d4cd10671b06073e2f9336c950
juan759 <jasso.antonio2001@gmail.com>
2020-10-24 23:36:24 -0500

    Metodos de tipo de Esqueletos.

--- src/EsqueletoReforzado.java
+++ src/EsqueletoReforzado.java
@@ -3,7 +3,9 @@ public class EsqueletoReforzado extends Construccion{
     int PrecioEsqueletoReforzado=1000;
 
     public void construccionEsqueleto(){
-        
+        System.out.println("Construyendo el Esqueleto Reforzado");
+        Precio += PrecioEsqueletoReforzado;
+
     }
 
 }
